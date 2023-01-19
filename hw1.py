7c13770d9cc6e2e5dc2f529f10f2ddace554e36a
pscala22 <89755484+pscala22@users.noreply.github.com>
2023-01-19 14:02:16 -0500

    p1 changes

--- hw1.py
+++ hw1.py
@@ -14,10 +14,30 @@ import matplotlib.pyplot as plt
 print ("Problem 1: Quadratic Equation")
 
 #Asking for user input
-a = input('Enter "a" value = ')
-b = input('Enter "b" value = ')
-c = input('Enter "c" value = ')
+a = float(input('Enter "a" value = '))
+b = float(input('Enter "b" value = '))
+c = float(input('Enter "c" value = '))
 
+#Discriminant Value
+def findDis (a,b,c):
+    dis_value = b*b-4*a*c
+
+    print("Discriminant value = ") + dis_value
+
+    if dis_value > 0:
+        print ("Discriminant is greater than 0")
+
+    elif dis_value == 0:
+        print ("Discriminant is equal to 0")
+
+    elif dis_value < 0:
+        print ("Discriminant is less than 0")
+
+    else:
+        print ("Unknown Error")
+
+findDis(a,b,c)
+"""
 #2 Pythagorean Numbers
 print ("Problem 2: Pythagorean Numbers")
 
@@ -28,3 +48,4 @@ print ("Problem 3: Duplicated Substrings")
 
 #4 Function Visualization
 print ("Problem 4: Function Visualization")
+"""
