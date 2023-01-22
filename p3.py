a8e50a64725f3b7975fa0afbef6087cb41c57383
pscala22 <89755484+pscala22@users.noreply.github.com>
2023-01-22 18:06:41 -0500

    p3 before comments

--- p3.py
+++ p3.py
@@ -12,3 +12,29 @@ import matplotlib.pyplot as plt
 
 #3 Duplicated Substrings
 print ("Problem 3: Duplicated Substrings")
+
+def find_dup_str(s, n):
+    for i in range(len(s)-n+1):
+        substring = s[i:i+n]
+        if s.count(substring) > 1:
+            return substring
+    return ""
+    
+
+def find_max_dup(s):
+    result = ""
+    for n in range(len(s), 0, -1):
+        temp = find_dup_str(s, n)
+        if temp:
+            result = temp
+            break
+    return result
+
+#main
+while True:
+    s = input("Enter a string: ")
+    if not s:
+        break
+    n = int(input("Enter a number: "))
+    print(("Dup_String "),(find_dup_str(s, n)))
+    print(("Max_Dup_String"),(find_max_dup(s)))
