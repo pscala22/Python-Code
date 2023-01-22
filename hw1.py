051306aa2a97c070ea1fb1b336ed8df2fcc0eff9
pscala22 <89755484+pscala22@users.noreply.github.com>
2023-01-22 16:21:51 -0500

    p1 changes

--- hw1.py
+++ hw1.py
@@ -13,41 +13,40 @@ import matplotlib.pyplot as plt
 
 #1 Quadratic Equation
 print ("Problem 1: Quadratic Equation")
+while True:
+    #Asking for user input
+    a = float(input('Enter "a" value = '))
+    if not a:
+        break
+    b = float(input('Enter "b" value = '))
+    c = float(input('Enter "c" value = '))
 
-#Asking for user input
-a = float(input('Enter "a" value = '))
-b = float(input('Enter "b" value = '))
-c = float(input('Enter "c" value = '))
+    #Discriminant Value
+    if a != 0:
+        dis_value = b ** 2 - 4 * a * c
+        sqrt_val = math.sqrt(abs(dis_value))
 
-#Discriminant Value
-if a == 0:
-        print ("Discriminant is equal to 0")
-        print ("Error")
-else:
-    dis_value = b * b - 4 * a * c
-    sqrt_val = math.sqrt(abs(dis_value))
+        if dis_value > 0:
+            print ("Discriminant is greater than 0")
+            print ("Two Solutions: ")
+            print ((-b + sqrt_val)/(2 * a))
+            print ((-b - sqrt_val)/(2 * a))
 
-    if dis_value > 0:
-        print ("Discriminant is greater than 0")
-        print ("Real and different roots ")
-        print ((-b + sqrt_val)/(2 * a))
-        print ((-b - sqrt_val)/(2 * a))
+        elif dis_value == 0:
+            print ("Discriminant is equal to 0")
+            print ("One Solution")
+            print (-b / (2 * a))
 
-    elif dis_value == 0:
-        print ("Discriminant is equal to 0")
-        print ("Real with the same roots")
-        print (-b / (2 * a))
+        elif dis_value < 0:
+         print ("No Real Solutions") 
 
-    else:
-        print ("Discriminant is Complex") 
-        print (-b / (2 * a)), " + i"
-        print (-b / (2 * a)), " - i"
+        else: 
+            print ("Error")
 
-"""
 #2 Pythagorean Numbers
 print ("Problem 2: Pythagorean Numbers")
 
-
+"""
 #3 Duplicated Substrings
 print ("Problem 3: Duplicated Substrings")
 
