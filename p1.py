95f85350090f48aa0b2dc7c4f754754bbe6ce891
pscala22 <89755484+pscala22@users.noreply.github.com>
2023-01-22 22:37:39 -0500

    p1 changes

--- p1.py
+++ p1.py
@@ -12,39 +12,38 @@ import matplotlib.pyplot as plt
 
 #1 Quadratic Equation
 print ("Problem 1: Quadratic Equation")
+import math
+import matplotlib.pyplot as plt
+
 while True:
     #Asking for user input
-    a = float(input('Enter a: '))
+    a = input('Enter a: ')
     if not a:
         break
+    a = float(a)
     b = float(input('Enter b: '))
-    c = float(input('Enter 1c: '))
+    c = float(input('Enter c: '))
 
     #Discriminant Value
-    if a != 0:
-        dis_value = b ** 2 - 4 * a * c #calculating discriminant
-        sqrt_val = math.sqrt(abs(dis_value))
+    dis_value = b ** 2 - 4 * a * c #calculating discriminant
+    sqrt_val = math.sqrt(abs(dis_value))
 
-        if dis_value > 0:
-            x1 = ((-b + sqrt_val)/(2 * a))
-            x2 = ((-b - sqrt_val)/(2 * a))
-            print (("Two Solutions: x1="),("%.5f" % x1)," x2=","%.5f" % x2) #Printing solutions to 5 decimal places
+    if dis_value > 0:
+        x1 = ((-b + sqrt_val)/(2 * a))
+        x2 = ((-b - sqrt_val)/(2 * a))
+        print (("Two Solutions: x1="),("%.5f" % x1)," x2=","%.5f" % x2) #Printing solutions to 5 decimal places
 
-        elif dis_value == 0:
-            print ("Discriminant is equal to 0")
-            print ("One Solution")
-            x =  (-b / (2 * a))
-            print (("One Solution: "),("%.5f" % x)) #Printing solution to 5 decimal places
+    elif dis_value == 0:
+        x =  (-b / (2 * a))
+        print (("One Solution: "),("%.5f" % x)) #Printing solution to 5 decimal places
 
-        elif dis_value < 0:
-         print ("No Real Solutions") 
+    elif dis_value < 0:
+        print ("No Real Solutions") 
 
-        else: 
-            print ("Error")
-    
-        #Plotting Function  
-        x = [i/10 for i in range(-30, 31)] # 150 points
-        y = [a * i**2 + b * i + c for i in x]
+    #Plotting Function  
+    x = [i/10 for i in range(-30, 31)] # 150 points
+    y = [a * i**2 + b * i + c for i in x]
+
     if dis_value > 0:
         plt.xlim((min(x1, x2)-1, max(x1, x2)+1))
     else:
@@ -52,4 +51,3 @@ while True:
     plt.ylim((min(y)-1, max(y)+1)) # adjust the y axis limit
     plt.plot(x, y)
     plt.scatter(x, y)
-    plt.show()
